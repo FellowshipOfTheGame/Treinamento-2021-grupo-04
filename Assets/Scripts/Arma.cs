c72632359e1a1fc8b47bf5fbe38f8c6fdceff444
Thales Darini Souza <59424479+thalesdarini@users.noreply.github.com>
2021-09-04 01:23:00 -0300

    Initial knockback

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Arma : MonoBehaviour {
-    public GameObject projetilPrefab;
+    [SerializeField] GameObject projetilPrefab;
+    [SerializeField] Transform shotPosition;
     GameObject projetil;
-    // Start is called before the first frame update
-    void Start() {
-        
+
+    PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
 
     public void Atirar(){
         if(!projetil){
-            projetil = Instantiate(projetilPrefab) as GameObject;
+            projetil = Instantiate(projetilPrefab, shotPosition.position, transform.rotation) as GameObject;
+            projetil.GetComponent<Projetil>().SetOwner(playerController.GetPlayer());
             Vector3 offset;
             offset.x = 1f;
             offset.y = 1f;
             offset.z = 0f;
-            projetil.transform.position = this.transform.position + offset;
+            //projetil.transform.position = this.transform.position + offset;
         }
     }
 }
