cebe5b1b6e23a2682ce57e96a339b9a55ba9fa56
Pedro <pedrostradioto@gmail.com>
2021-09-10 20:29:56 +0300

    Menu com opcionais

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,27 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public string sceneName;
+    public GameObject menuOptions;
+    public GameObject[] sliders;
+    private GameObject[] Buttons;
+
     private void Start()
     {
-        Time.timeScale = 1f;   
+        Buttons = GameObject.FindGameObjectsWithTag("Buttons");
+        Time.timeScale = 1f;
     }
     
     public void StartButton()
     {
-        SceneManager.LoadScene("Round");
+        SceneManager.LoadScene(sceneName);
         
     }
 
     public void OptionsButton()
+    {  
+        foreach (GameObject btn in Buttons)
+        {
+            sliders[0].GetComponent<Slider>().value = PlayerPrefs.GetFloat("MusicVolume"); //Update slide value when acessing the options.
+            sliders[1].GetComponent<Slider>().value = PlayerPrefs.GetFloat("EffectsVolume");
+            btn.SetActive(false);
+            menuOptions.SetActive(true);
+        }
+    }
+
+    public void SaveOptions()
     {
-        Debug.Log("Options selected");
+        foreach (GameObject btn in Buttons)
+        {
+            UpdateVolumeValue();
+            btn.SetActive(true);
+            menuOptions.SetActive(false);
+        }
     }
 
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    void UpdateVolumeValue()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", sliders[0].GetComponent<Slider>().value); //update pref volume when leaving options.
+        PlayerPrefs.SetFloat("EffectsVolume", sliders[1].GetComponent<Slider>().value);
     }
 }
