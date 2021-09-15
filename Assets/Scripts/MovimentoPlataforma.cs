19680eee0356f5cb78ac026c885e5bc29529e72b
Thales Darini Souza <59424479+thalesdarini@users.noreply.github.com>
2021-09-15 13:40:28 -0300

    Title and background images, bug fixes and UI scaling
    - fixed movimento das plataformas (amplitude funciona corretamente agr)
    - UI escala com resoluções diferentes corretamente
    - player não colide com lateral das plataformas

--- Assets/Scripts/MovimentoPlataforma.cs
+++ Assets/Scripts/MovimentoPlataforma.cs
@@ -5,11 +5,10 @@ using System;
 
 public class MovimentoPlataforma : MonoBehaviour
 {
-    public GameObject PauseScreenObject;
-    public GameObject EndScreenObject;
+    //public GameObject PauseScreenObject;
+    //public GameObject EndScreenObject;
 
     private float yOffSet;
-
     public float freqY = 1f;
     public float ampY = 0.0001f;
 
@@ -17,19 +16,29 @@ public class MovimentoPlataforma : MonoBehaviour
     public float freqX = 1f;
     public float ampX = 0f;
 
+    private Vector2 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        yOffSet = UnityEngine.Random.value * 3.14f;
-        xOffSet = UnityEngine.Random.value * 3.14f;
+        xOffSet = UnityEngine.Random.value * Mathf.PI + Mathf.PI / 2; // somando com pi/2 para que o seno varie de 1 a -1 com este offset inicial
+        yOffSet = UnityEngine.Random.value * Mathf.PI + Mathf.PI / 2;
+
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!PauseScreenObject.activeInHierarchy && !EndScreenObject.activeInHierarchy)
+        /*if (!PauseScreenObject.activeInHierarchy && !EndScreenObject.activeInHierarchy)
         {
             transform.Translate(new Vector2((float)Math.Sin(freqX * (Time.time + xOffSet)) * ampX, (float)Math.Sin(freqY * (Time.time + yOffSet)) * ampY));
-        }
+        }*/
+
+        Vector2 newPosition = new Vector2(Mathf.Sin(freqX * (Time.time + xOffSet)) * ampX, Mathf.Sin(freqY * (Time.time + yOffSet)) * ampY);
+        newPosition += startPos; // sempre relativo à posição inicial
+
+        // Translate recebe vetor direção que é a posição q desejamos chegar menos a que estamos
+        transform.Translate(newPosition - (Vector2) transform.position); 
     }
 }
